58b6fdbb570aae7cc6fa25c1f1341298c90bb1aa
Don Pugsley <donald.e.pugsley.civ@us.navy.mil>
2024-05-16 13:08:27 -0400

    Working stream iteration with realtime plot to 20 Hz in vmrftmg.py

--- vmrftmg.py
+++ vmrftmg.py
@@ -2,15 +2,43 @@ from pylive import live_plotter,live_plotter_xy
 import numpy as np
 import tldevicesync
 
+def getTimebase(dev):
+  timebase_id = dev.vmr0._tio.protocol.streamInfo['stream_timebase_id']
+  tb0 = dev.vmr0._tio.protocol.timebases[timebase_id]['timebase_Fs']
+  timebase_id = dev.vmr1._tio.protocol.streamInfo['stream_timebase_id']
+  tb1 = dev.vmr1._tio.protocol.timebases[timebase_id]['timebase_Fs']
+  timebase_id = dev.vmr2._tio.protocol.streamInfo['stream_timebase_id']
+  tb2 = dev.vmr2._tio.protocol.timebases[timebase_id]['timebase_Fs']
+
+  if tb0 != tb1 | tb0 != tb2:
+    print('Please fix VMR timebases, they are not all the same {tb0}, {tb1}, {tb2}')
+    exit
+  return tb1
+
+# At 100 this doesn't keep up... 20 Seems OK
+SR = 20 # VMR Sampling rate
+WINDOWSEC = 10 # Seconds of data in plot window
+GETSEC = 1.0 # Get this many seconds of data at a time
+FRAMEINTERVALMS = 1.0/GETSEC # milliseconds between draw events
+
 tio = tldevicesync.DeviceSync()
 
 streams = []
-# streams += [tio.vmr.vector] # Single VMR
 streams += [tio.vmr0.vector]
 streams += [tio.vmr1.vector]
 streams += [tio.vmr2.vector]
 ss = tldevicesync.SyncStream(streams)
 
+# Set sampling rate on all VMRs
+timebasehz = getTimebase(tio)
+decimation = int(timebasehz/SR)
+print(f'Setting sampling rate (decimation = {timebasehz}/{SR} = {decimation})')
+tio.vmr0.vector.data.decimation(decimation)
+tio.vmr1.vector.data.decimation(decimation)
+tio.vmr2.vector.data.decimation(decimation)
+print(f'Sampling rate set to {tio.vmr2.data.rate()}')
+
+# Choose one axis for plotting
 k = 1; 
 print('Using ', ss.columnnames()[k])
 
